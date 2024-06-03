a94c2bb0d440644db8b0312cbabefc65938485ee
Konstantinos Pretsios <100194348+Konstantinos2421@users.noreply.github.com>
2024-06-03 18:56:57 +0300

    Update heap_sort-counting_sort.cpp

--- sorting_algorithms/heap_sort-counting_sort.cpp
+++ sorting_algorithms/heap_sort-counting_sort.cpp
@@ -37,14 +37,14 @@ void read_data(ifstream &fin, Data *table){
 
 
 Data* Heapify_Array(Data *table, int length){
-    Data *heap = new Data[length+1];  //Δημιουργία νέου δυναμικού πίνακα heap ο οποίος χρησιμοποιείται ως σωρός
+    Data *heap = new Data[length+1];  //Creation of new dynamic table which is used as a heap
 
-    //Παρακάτω γίνεται εισαγωγή των στοιχείων του table στον heap και μετά από κάθε εισαγωγή γίνεται αποκατάστση της δομής του σωρού
+    //Table elements are imported into the heap and after each import the heap structure is restored
     for(int i=1; i<=length; i++){
-        heap[i] = table[i-1];  //Εισαγωγή κάθε στοιχείου του table στο τέλος του heap 
+        heap[i] = table[i-1];  //Each element of the table is inserted at the end of the heap 
         int j = i;
-        while(heap[j].phosphate > heap[j/2].phosphate && j > 1){ // Αν η τιμή phosphate του πατέρα είναι μικρότερη από αυτή του γιου δηλαδή του στοιχείου που εισαγάγαμε
-            Data temp = heap[j];                                 // τότε γίνεται εναλλαγή των δύο στοιχείων. Ο έλεγχος γίνεται μέχρι να φτάσουμε στο πρώτο στοιχείο.
+        while(heap[j].phosphate > heap[j/2].phosphate && j > 1){ // If the phosphate value of the father is lower than that of the son, i.e. the element that was just inserted,
+            Data temp = heap[j];                                 // then the two elements are swapped. The check is done until the first element is reached.
             heap[j] = heap[j/2];
             heap[j/2] = temp;
             j = j/2;
@@ -55,42 +55,42 @@ Data* Heapify_Array(Data *table, int length){
 }
 
 
-void Heap_Sort(Data *table, int length){
-    Data *heap = Heapify_Array(table, length); //Φέρνω τον πίνακα table σε μορφή σωρού με χρήση της παραπάνω συνάρτησης
+void Heap_Sort(Data *table, int length){I store in each position of the table the
+    Data *heap = Heapify_Array(table, length); //The table is converted into heap form
     int heap_length = length;
 
-    for(int i=length-1; i>=0; i--){  //Ξεκινώντας από το τέλος του πίνακα table αποθηκεύω σε κάθε θέση του
-        table[i] = heap[1];          //το πρώτο στοιχείο του σωρού το οποίο είναι το στοιχείο με τη μεγαλύτερη τιμή phosphate
-        Data temp = heap[1];         //Εναλάσσω το πρώτο με το τελευταίο στοιχείο του σωρού
+    for(int i=length-1; i>=0; i--){  //Starting from the end of the table, in each of its cells is stored the
+        table[i] = heap[1];          //first element of the heap which is the element with the highest phosphate value
+        Data temp = heap[1];         //First and last element of the heap are swapped
         heap[1] = heap[heap_length];
         heap[heap_length] = temp;
-        heap_length--;               //Μειώνω το μέγεθος του σωρού κατά 1 ώστε να γίνει εξαγωγή του στοιχείου με τη μεγαλύτερη τιμή phosphate
+        heap_length--;               //The size of the heap is reduced by 1 to extract the element with the highest phosphate value
         
-        //Στη συνέχεια γίνεται αποκατάσταση της δομής του σωρού μετά την εναλλαγή του πρώτου και του τελευταίου στοιχείου
+        //The structure of the heap is then restored after the first and last elements have been swaped
         int j = 1;
-        while(2*j < heap_length){ //Ξεκινώντας από τη ρίζα, όσο ο πατέρας έχει δύο παιδιά εκτελείται η παρακάτω διαδικασία
-            if(heap[j].phosphate < heap[2*j].phosphate || heap[j].phosphate < heap[2*j+1].phosphate){ //Αν η τιμή phosphate του πατέρα είναι μικρότερη από αυτή τουλάχιστον ενός από τους δύο γιους του εκτελείται η παρακάτω διαδικασία
-                if(heap[2*j].phosphate > heap[2*j+1].phosphate){ //Αν η τιμή phosphate του δεξιού γιου είναι μεγαλύτερη από αυτή του αριστερού γιου
-                    Data temp = heap[j];                         //γίνεται εναλλαγή του πατέρα με τον δεξί γιο του
+        while(2*j < heap_length){ //Starting from the root, as long as the father has two children, the following procedure is carried out
+            if(heap[j].phosphate < heap[2*j].phosphate || heap[j].phosphate < heap[2*j+1].phosphate){ //If the phosphate value of the father is lower than that of at least one of his two sons the following procedure is performed
+                if(heap[2*j].phosphate > heap[2*j+1].phosphate){ //If the phosphate value of the right son is higher than that of the left son
+                    Data temp = heap[j];                         //the father is swapped with his right son
                     heap[j] = heap[2*j];
                     heap[2*j] = temp;
-                    j = 2*j;            //Μεταβάλλουμε το j ώστε να μεταβούμε στον δεξί γιο
-                }else if(heap[2*j].phosphate < heap[2*j+1].phosphate){ //Αν η τιμή phosphate του αριστερού γιου είναι μεγαλύτερη από αυτή του δεξιού γιου
-                    Data temp = heap[j];                               //γίνεται εναλλαγή του πατέρα με τον αριστερό γιο του
+                    j = 2*j;            //j is changed to go to the right son
+                }else if(heap[2*j].phosphate < heap[2*j+1].phosphate){ //If the phosphate value of the left son is higher than that of the right son
+                    Data temp = heap[j];                               //the father is swapped with his left son
                     heap[j] = heap[2*j+1];
                     heap[2*j+1] = temp;
-                    j = 2*j + 1;          //Μεταβάλλουμε το j ώστε να μεταβούμε στον αριστερό γιο
-                }else{                    //Αλλιώς αν οι τιμές phosphate του δεξιού και του αριστερού γιου είναι ίσες
-                    Data temp = heap[j];  //γίνεται τυχαία εναλλαγή του πατέρα με τον δεξί γιο του
+                    j = 2*j + 1;          //j is changed to go to the left son
+                }else{                    //Otherwise, if the phosphate values of the right and left sons are equal
+                    Data temp = heap[j];  //there is a random switching of the father with his right son
                     heap[j] = heap[2*j];
                     heap[2*j] = temp;
-                    j = 2*j;              //Μεταβάλλουμε το j ώστε να μεταβούμε στον δεξί γιο
+                    j = 2*j;              //j is changed to go to the right son
                 }
-            }else break; //Αν η τιμή phosphate του πατέρα είναι μεγαλύτερη από τις τιμές phosphate και των δύο γιων του τότε σταματάει η επανάληψη αφού έχει αποκατασταθεί η δομή του σωρού
+            }else break; //If the phosphate value of the father is greater than the phosphate values of both his sons then the iteration stops after the heap structure has been restored
         }
 
-        if(2*j == heap_length && heap[j].phosphate < heap[2*j].phosphate){ //Ελέγχω την περίπτωση που ο πατέρας έχει μόνο έναν γιο. Αν ο γιος του έχει μεγαλύτερη τιμή phosphate
-            Data temp = heap[j];                                           //τότε γίνεται εναλλαγή των στοιχείων
+        if(2*j == heap_length && heap[j].phosphate < heap[2*j].phosphate){ //I check the case where the father has only one son
+            Data temp = heap[j];                                           //If his son has a higher phosphate value then the elements are swapped
             heap[j] = heap[2*j];
             heap[2*j] = temp;
         }
@@ -99,30 +99,31 @@ void Heap_Sort(Data *table, int length){
 
 
 void Counting_Sort(Data *table, int length, int wide){
-    Data *tableB = new Data[length]; //Δημιουργία βοηθητικού πίνακα tableB στον οποίο αποθηκεύονται αντικείμενα τύπου Data 
-    int *tableC = new int[wide+1];   //Δημιουργία βοηθητικού πίνακα tableC στον οποίο αποθηκεύονται ακέραιοι
+    Data *tableB = new Data[length]; //Creation of an auxiliary tableB in which objects of type Data are stored
+    int *tableC = new int[wide+1];   //Creation of an auxiliary tableC in which integers are stored
 
-    for(int i=0; i<wide+1; i++)  //Αρχικοποίηση όλων των θέσεων του πίνακα tableC με 0
+    for(int i=0; i<wide+1; i++)  //Initializion of all positions of tableC with 0
         tableC[i] = 0;
 
-    for(int i=0; i<length; i++)                //Για κάθε στοιχείο του πίνακα table πολλαπλασιάζουμε την τιμή phosphate με 100 για να μετατραπεί σε ακέραιο
-        tableC[int(table[i].phosphate*100)]++; //και αυξάνουμε κατά 1 τον αντίστοιχο μετρητή στον πίνακα tableC
+    for(int i=0; i<length; i++)                //For each element of the table the phosphate value is multiplied by 100 to be converted into an integer
+        tableC[int(table[i].phosphate*100)]++; //and the corresponding counter in tableC is incremented by 1
 
-    for(int i=1; i<wide+1; i++)   //Σε κάθε θέση του πίνακα tableC αποθηκεύουμε το άθροισμα του περιεχομένου
-        tableC[i] += tableC[i-1]; //της συγκεκριμένης θέσης και του περιεχομένου της προηγούμενής της ξεκινώντας από τη θέση 1 του πίνακα
+    for(int i=1; i<wide+1; i++)   //In each position of tableC we store the sum of the contents of that position and the
+        tableC[i] += tableC[i-1]; //contents of its previous position, starting from position 1 of the table
 
-    for(int i=length-1; i>=0; i--){              //Για κάθε στοιχείο του table
-        int index = int(table[i].phosphate*100); //βρίσκουμε τη σωστή θέση εισαγωγής του στον ταξινομημένο πίνακα tableB όπως προσδιορίζεται από τον πίνακα tableC
-        tableB[tableC[index]-1] = table[i];      //και αντιγράφουμε το στοιχείο στον πίνακα tableB
-        tableC[index]--;                         //μειώνοντας τον αντίστοιχο μετρητή στον tableC κατά 1
+    for(int i=length-1; i>=0; i--){              //For each element of the table we find its correct insertion position in the sorted tableB as determined
+        int index = int(table[i].phosphate*100); //by tableC and copy the element to tableB by decreasing the corresponding counter in tableC by 1
+        tableB[tableC[index]-1] = table[i];
+        tableC[index]--;
     }
 
-    for(int i=0; i<length; i++) //Αποθηκεύω τα ταξινομημένα στοιχεία του πίνακα tableB στον πίνακα table
+    for(int i=0; i<length; i++) //The sorted data of tableB are stored in the table
         table[i] = tableB[i];
 }
 
 
-void display_data(Data *table, int length){  //Εκτύπωση των δεδομένων των στοιχείων του πίνακα table στο αρχείο "sorted_data.doc"
+//Print data of the table elements in the file "sorted_data.doc"
+void display_data(Data *table, int length){ 
     ofstream fout("sorted_data.doc");
 
     fout << "Date\t\t\tPhosphate" << endl;
